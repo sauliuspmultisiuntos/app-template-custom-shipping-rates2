5c8ae09d193a5473d2e84a4a6e9afcd12f586a22
sauliuspmultisiuntos <saulius@multisuntos.lt>
2024-11-08 09:59:39 +0200

    Your commit message here

--- src/app/api/shipping-rates/v1/getRates/route.ts
+++ src/app/api/shipping-rates/v1/getRates/route.ts
@@ -1,28 +1,27 @@
-
 import { getShippingAppData } from '@/app/actions/app-data';
 import { calculatePrice } from '@/app/utils/shipping-calculator';
 import { wixAppClient } from '@/app/utils/wix-sdk.app';
 
 wixAppClient.shippingRates.provideHandlers({
   async getShippingRates({ request, metadata }) {
-    const appData = await getShippingAppData({ instanceId: metadata.instanceId! });
-    const currency = metadata.currency;
-
-    return {
-      shippingRates: appData.shippingMethods.map(({ code, title, logistics, costs, unitOfMeasure }) => ({
-        code,
-        title: title === 'Standard Shipping' ? 'Standard Shipping 4' : title,
-        logistics,
-        cost: {
-          price: `${calculatePrice(request, costs, unitOfMeasure)}`,
-          currency: currency!,
-        },
-      })),
-    };
+  const appData = await getShippingAppData({ instanceId: metadata.instanceId! });
+  const currency = metadata.currency;
+  
+  return {
+  shippingRates: appData.shippingMethods.map(({ code, title, logistics, costs, unitOfMeasure }) => ({
+  code,
+  title: title === 'Standard Shipping' ? 'Standard Shipping 4' : title,
+  logistics,
+  cost: {
+  price: `${calculatePrice(request, costs, unitOfMeasure)}`,
+  currency: currency!,
   },
-});
-
+  })),
+  };
+  },
+  });
 export async function POST(request: Request) {
   console.info('Shipping rates::POST - called');
   return wixAppClient.servicePlugins.processRequest(request);
-}
+  }
+  
