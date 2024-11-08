efb90444d86747f16c8314856767e964867274d2
sauliuspmultisiuntos <saulius@multisuntos.lt>
2024-11-08 11:46:09 +0200

    Your commit message here a a

--- src/app/client-hooks/access-token.ts
+++ src/app/client-hooks/access-token.ts
@@ -1,11 +1,14 @@
-'use client';
+// src/app/client-hooks/access-token.ts
+
 import { useSDK } from '@/app/utils/wix-sdk.client-only';
 import { useSearchParams } from 'next/navigation';
 
-export const useAccessToken = () => {
+export const getAccessToken = () => {
   const { dashboard } = useSDK();
   const searchParams = useSearchParams();
-  return searchParams.get('accessToken')
+  
+  // Use optional chaining to handle possible null value
+  return searchParams?.get('accessToken')
     ? Promise.resolve(searchParams.get('accessToken'))
     : dashboard.getAccessToken?.();
 };
