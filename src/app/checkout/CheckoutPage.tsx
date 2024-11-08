965146ed951e37800430fe50b3ddf10bdf42911b
sauliuspmultisiuntos <saulius@multisuntos.lt>
2024-11-08 15:17:58 +0200

    Your commit Pop up app

--- src/app/checkout/CheckoutPage.tsx
+++ src/app/checkout/CheckoutPage.tsx
@@ -1,15 +1,13 @@
 
-import { DemoDropdown } from '@/app/checkout/DemoDropdown';
 import React from 'react';
 
 export function CheckoutPage() {
+  console.log("CheckoutPage component loaded"); // Debug log
+
   return (
-    <div>
-      <h1>Checkout Page</h1>
-      <DemoDropdown />
-      {/* Other checkout content */}
+    <div style={{ padding: '20px', backgroundColor: '#ffe4b5', textAlign: 'center' }}>
+      <h1>Custom Checkout Page</h1>
+      {/* The rest of your checkout page content goes here */}
     </div>
   );
 }
-
-export default CheckoutPage;
