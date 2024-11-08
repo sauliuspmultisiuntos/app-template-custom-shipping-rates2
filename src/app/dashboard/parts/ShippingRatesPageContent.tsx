fcb633c22c6edf6f65a067d996d410e43f2cafbb
sauliuspmultisiuntos <saulius@multisuntos.lt>
2024-11-08 10:53:58 +0200

    Your commit message here a a

--- src/app/dashboard/parts/ShippingRatesPageContent.tsx
+++ src/app/dashboard/parts/ShippingRatesPageContent.tsx
@@ -1,92 +1,36 @@
-import { Box, Breadcrumbs, Button, Cell, Layout, Loader, Page } from '@wix/design-system';
-import { useSDK } from '@/app/utils/wix-sdk.client-only';
-import { useCallback, useEffect, useState } from 'react';
-import { ShippingDeliveryMethodForm } from '@/app/dashboard/parts/ShippingDeliveryMethodForm';
-import { WixPageId } from '@/app/utils/navigation.const';
-import { useSetShippingAppData, useShippingAppData } from '@/app/client-hooks/app-data';
-import testIds from '@/app/utils/test-ids';
+'use client';
+import { dashboard, SDK } from '@wix/dashboard';
+import { useMemo } from 'react';
+import { createClient } from '@wix/sdk/client';
 
-export const ShippingRatesPageContent = () => {
-  const {
-    dashboard: { showToast, navigate },
-  } = useSDK();
-  const persistShippingAppData = useSetShippingAppData();
-  const { data: persistedShippingAppData, isLoading: isLoadingAppData } = useShippingAppData();
-  const [currentShippingAppData, setCurrentShippingAppData] = useState(persistedShippingAppData);
+function inIframe() {
+  try {
+    return window.self !== window.top;
+  } catch (e) {
+    return true;
+  }
+}
 
-  useEffect(() => {
-    setCurrentShippingAppData(persistedShippingAppData);
-  }, [persistedShippingAppData]);
+export const useSDK = () => {
+  // Extract complex dependency
+  const isWindowDefined = typeof window !== 'undefined';
 
-  const [loading, setLoading] = useState(false);
-  const onSave = useCallback(() => {
-    setLoading(true);
-    persistShippingAppData(currentShippingAppData!)
-      .then(() => {
-        showToast({ message: 'Shipping rates saved successfully.', type: 'success' });
-      })
-      .catch(() => {
-        showToast({ message: 'Failed to save shipping rates.', type: 'error' });
-      })
-      .finally(() => setLoading(false));
-  }, [persistShippingAppData, currentShippingAppData, showToast]);
-
-  const ButtonsBar = useCallback(
-    () => (
-      <Box gap='SP2'>
-        <Button
-          skin='standard'
-          priority='secondary'
-          onClick={() => setCurrentShippingAppData(persistedShippingAppData)}
-        >
-          Cancel
-        </Button>
-        <Button onClick={onSave}>{loading ? <Loader size='tiny' /> : 'Save'}</Button>
-      </Box>
-    ),
-    [loading, onSave, persistedShippingAppData],
+  const sdk = useMemo(
+    () =>
+      !isWindowDefined || !inIframe()
+        ? {
+            // The SDK is not initialized during server-side rendering or outside an iframe, making SDK methods unusable in these contexts.
+            dashboard: {} as SDK,
+          }
+        : createClient({
+            host: dashboard.host(),
+            auth: dashboard.auth(),
+            modules: {
+              dashboard,
+            },
+          }),
+    [isWindowDefined] // Updated dependency array
   );
 
-  return (
-    <Page height='100vh' dataHook={testIds.DASHBOARD.WRAPPER}>
-      <Page.Header
-        actionsBar={<ButtonsBar />}
-        breadcrumbs={
-          <Breadcrumbs
-            activeId='2'
-            items={[
-              { id: WixPageId.MANAGE_APPS, value: 'Apps' },
-              { id: 'shipping-app-page', value: 'Shipping Rate App', disabled: true },
-            ]}
-            onClick={({ id }) => navigate(id as string)}
-          />
-        }
-        title='Shipping Rate App'
-        subtitle='Customize shipping fees based on item quantity, weight, and delivery speed, ensuring a flexible and cost-effective solution for your business.'
-      />
-      <Page.Content>
-        <Layout>
-          <Cell span={8}>
-            {isLoadingAppData ? (
-              <Loader size='large' />
-            ) : (
-              currentShippingAppData?.shippingMethods.map((method, index) => (
-                <Cell key={method.code}>
-                  <ShippingDeliveryMethodForm
-                    expandByDefault={index === 0}
-                    title={method.title}
-                    unitOfMeasure={method.unitOfMeasure}
-                    onUnitOfMeasureSelected={() => {}}
-                    shippingCosts={method.costs}
-                    onShippingCostsChanged={() => {}}
-                    methodType={method.type}
-                  />
-                </Cell>
-              )),
-            )}
-          </Cell>
-        </Layout>
-      </Page.Content>
-    </Page>
-  );
+  return sdk;
 };
