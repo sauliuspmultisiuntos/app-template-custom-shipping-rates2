3dd6849b901af3ba69041769cfe37d9fe541f3be
sauliuspmultisiuntos <saulius@multisuntos.lt>
2024-11-11 14:55:13 +0200

    Your commit Pop up app

--- src/app/checkout/CheckoutPage.tsx
+++ src/app/checkout/CheckoutPage.tsx
@@ -4,7 +4,7 @@ function CheckoutPage() {
   return (
     <div>
       {/* Existing checkout and order summary elements */}
-      <OrderSummary />
+      
       
       {/* Demo text block */}
       <DemoTextBlock />
